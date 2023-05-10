99508f3589ae737901f81c5154373774d18c3ac0
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-05-10 20:20:32 +0600

    project setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				satoshi: ['Satoshi', 'sans-serif'],
+				inter: ['Inter', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [],
 };
