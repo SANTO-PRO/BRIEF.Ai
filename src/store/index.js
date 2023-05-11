818352e65f31e6122073caa0543d7299ca81031b
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-05-11 09:01:24 +0600

    Redux Store Design

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-
+import { setupListeners } from '@reduxjs/toolkit/query/react';
 const store = configureStore({
 	reducer: {
 		//
 	},
+	middleware: () => {},
 });
+
+setupListeners(store);
 
 export { store };
