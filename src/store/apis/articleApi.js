2c7bf7778de9ed14e60b8e2b480033d1dbfcfd26
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-05-11 09:41:03 +0600

    Article Api create

--- src/store/apis/articleApi.js
+++ src/store/apis/articleApi.js
@@ -1,15 +1,36 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+const options = {
+	method: 'GET',
+	url: 'https://article-extractor-and-summarizer.p.rapidapi.com/summarize',
+	params: {
+		url: 'https://time.com/6266679/musk-ai-open-letter/',
+		length: '3',
+	},
+	headers: {
+		'X-RapidAPI-Key': '5ee2a39dc0mshd55c54002ad467cp175c30jsn44026b3055e8',
+		'X-RapidAPI-Host': 'article-extractor-and-summarizer.p.rapidapi.com',
+	},
+};
+
 const articleApi = createApi({
 	reducerPath: 'articles',
 
 	baseQuery: fetchBaseQuery({
-		baseUrl: '',
+		baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
+
+		prepareHeaders: (headers) => {
+			headers.set('X-RapidAPI-Key', 'KEY');
+			headers.set(
+				'X-RapidAPI-Host',
+				'article-extractor-and-summarizer.p.rapidapi.com',
+			);
+		},
 	}),
 
 	endpoints: (builder) => {
 		return {
-			fetchArticles: builder.query({
+			getSummary: builder.query({
 				query: () => {
 					return {
 						url: '',
