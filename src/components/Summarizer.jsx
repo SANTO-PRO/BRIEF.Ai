6430130d904ab7a25db27a204af7671f1a5d0f56
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-05-11 08:19:06 +0600

    Summarizer Component Work

--- src/components/Summarizer.jsx
+++ src/components/Summarizer.jsx
@@ -1,8 +1,27 @@
-// import { TbCopy, TbLink, TbCheck } from 'react-icons/tb';
+import { TbCornerDownLeft, TbLink } from 'react-icons/tb';
 // import { loader } from '../assets';
 
 const Summarizer = () => {
-	return <div>Summarizer</div>;
+	return (
+		<section className="mt-16 w-full max-w-xl">
+			<div className="flex flex-col w-full gap-2">
+				<form
+					className="relative justify-center items-center"
+					onSubmit={() => {}}
+				>
+					<TbLink />
+					<input />
+
+					<button>
+						<TbCornerDownLeft />
+					</button>
+				</form>
+			</div>
+		</section>
+	);
 };
 
 export default Summarizer;
+
+// TbCopy
+// TbCheck
