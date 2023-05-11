0c009856af416c74e6bf4559a6d70cb4ec8df35d
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-05-11 08:56:02 +0600

    Redux Store Design

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App.jsx';
+import { store } from './store';
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<Provider>
+		<Provider store={store}>
 			<App />
 		</Provider>
 	</React.StrictMode>,
