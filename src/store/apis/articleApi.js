b58d96efff87be2ce89ca869c4a55d4392a8f723
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-05-11 09:11:06 +0600

    Article Api create

---
+++ src/store/apis/articleApi.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const articleApi = createApi({
+	reducerPath: 'articles',
+
+	baseQuery: fetchBaseQuery({
+		baseUrl: '',
+	}),
+
+	endpoints: (builder) => {
+		return {
+			fetchArticles: builder.query({
+				query: () => {
+					return;
+				},
+			}),
+		};
+	},
+});
+
+export { articleApi };
